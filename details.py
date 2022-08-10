18358c18c2791124e425c1b5b7593536e40c3d7e
Stranger215 <110084988+Stranger215@users.noreply.github.com>
2022-08-10 20:49:14 +0530

    Update details.py

--- details.py
+++ details.py
@@ -1,4 +1,4 @@
 api_id = 18516364
 api_hash = "2e90970ea9e3ad55a3d7566754274fae"
-bot_token = "2057624136:AAGqDUgyUmtNZjeaO-ySc-YYJhTomhS1hmA"
-Sudo_groups = ""
+bot_token = "5423943589:AAFR0NcTidYP1GxZqjnUcGgF4mQpWasXb0Y"
+Sudo_groups = "881816997,810759821"
