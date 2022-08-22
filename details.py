68fd679ec0727fd6f0d93a8e1348e7139ff92a45
Stranger215 <110084988+Stranger215@users.noreply.github.com>
2022-08-22 15:02:01 +0530

    Update details.py

--- details.py
+++ details.py
@@ -1,4 +1,4 @@
 api_id = 18516364
 api_hash = "2e90970ea9e3ad55a3d7566754274fae"
 bot_token = "5423943589:AAFR0NcTidYP1GxZqjnUcGgF4mQpWasXb0Y"
-Sudo_groups = "881816997,810759821,619116510"
+Sudo_groups = "881816997,810759821"
