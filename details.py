6522632ce0aa536baa943e0d4793de523d0c8793
nkhash <103872134+nkhash@users.noreply.github.com>
2023-01-07 19:52:47 +0530

    Update details.py

--- details.py
+++ details.py
@@ -1,4 +1,4 @@
-api_id = 5360733666
+api_id = 7830931
 api_hash = "affddad222632dbe581fde6d780ecc18"
 bot_token = "5772816206:AAHkCX40CcwnPWV1tg4jgOrJYMKAXJvclXs"
 Sudo_groups = "881816997,810759821"
